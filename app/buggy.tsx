4703c4020886d68f03bfe7f7df9baad49b44e137
Amanda <amandabcarneiro9@gmail.com>
2024-11-26 15:29:08 +0100

    add  a bug

--- app/buggy.tsx
+++ app/buggy.tsx
@@ -16,3 +16,32 @@ if (a !== NaN) {
 	// Noncompliant: Always true
 	console.log('a is not NaN');
 }
+
+// const users = [
+// 	{ name: 'Alice', age: 25 },
+// 	{ name: 'Bob', age: 30 },
+// 	{ name: 'Charlie', age: 20 },
+// ];
+
+// function sortUsersByAge(users: any[]) {
+//     return users.sort();
+// }
+// console.log(sortUsersByAge(users));
+
+function sortUsersByAge(users: { age: number }[]) {
+	return users.sort((a: { age: number }, b: { age: number }) => a.age - b.age);
+}
+
+const users = [
+	{ name: 'Alice', age: 25 },
+	{ name: 'Bob', age: 30 },
+	{ name: 'Charlie', age: 20 },
+];
+
+console.log(sortUsersByAge(users));
+
+// Correct output: [
+//   { name: "Charlie", age: 20 },
+//   { name: "Alice", age: 25 },
+//   { name: "Bob", age: 30 }
+// ]
